5697ebca4846db62a29c49502dda2a2e4188bc0b
Anthony Walter <sysrpl@gmail.com>
2023-02-01 10:57:00 -0500

    Added node free calls fixing issue #23

--- jsontools.pas
+++ jsontools.pas
@@ -941,6 +941,7 @@ begin
   N := Child(Index);
   if N <> nil then
   begin
+    N.Free;
     FList.Delete(Index);
     if FList.Count = 0 then
     begin
@@ -957,6 +958,7 @@ begin
   N := Child(Name);
   if N <> nil then
   begin
+    N.Free;
     FList.Remove(N);
     if FList.Count = 0 then
     begin
